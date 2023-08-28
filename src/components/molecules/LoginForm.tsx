e6c5cdf6b96e93cdf9db676a9f389a5dd3ca3da6
bkdragon0228 <rlaqjarb2067@naver.com>
2023-08-28 17:18:16 +0900

    refactor : form 컴포넌트 수정

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   FormControl,
@@ -10,26 +10,53 @@ import {
 import CustomInput from '../atoms/Input';
 import ButtonList from './ButtonList';
 
-type LoginFormProps = FormControlProps;
+type LoginFormProps = Omit<FormControlProps, 'onSubmit'> & {
+  onSubmit: (value: LoginData) => void;
+};
+
+type LoginData = {
+  email: string;
+  password: string;
+};
 
 const LoginForm = (props: LoginFormProps) => {
+  const { onSubmit, ...rest } = props;
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = () => {
+    onSubmit({ email, password });
+  };
+
   return (
-    <FormControl display={'flex'} flexDirection={'column'} gap={6} {...props}>
-      <Box display={'flex'} flexDirection={'row'} gap={6}>
-        <FormLabel>이메일</FormLabel>
-        <CustomInput />
-      </Box>
-      <Box display={'flex'} flexDirection={'row'} gap={6}>
-        <FormLabel>비밀번호</FormLabel>
-        <CustomInput type="password" />
-      </Box>
-      <ButtonList
-        buttonList={[
-          { children: '로그인', type: 'submit' },
-          { children: '회원가입', onClick: () => {}, type: 'button' },
-        ]}
-      />
-    </FormControl>
+    <form onSubmit={handleSubmit}>
+      <FormControl display={'flex'} flexDirection={'column'} gap={6} {...rest}>
+        <Box display={'flex'} flexDirection={'row'} gap={6}>
+          <FormLabel>이메일</FormLabel>
+          <CustomInput
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Box>
+        <Box display={'flex'} flexDirection={'row'} gap={6}>
+          <FormLabel>비밀번호</FormLabel>
+          <CustomInput
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Box>
+        <ButtonList
+          buttonList={[
+            { children: '로그인', type: 'submit' },
+            { children: '회원가입', onClick: () => {}, type: 'button' },
+          ]}
+        />
+      </FormControl>
+    </form>
   );
 };
 
--- src/components/molecules/RegisterForm.tsx
+++ src/components/molecules/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   FormControl,
@@ -11,21 +11,48 @@ import CustomInput from '../atoms/Input';
 
 import CustomButton from '../atoms/Button';
 
-type RegisterFormProps = FormControlProps;
+type RegisterFormProps = Omit<FormControlProps, 'onSubmit'> & {
+  onSubmit: (value: LoginData) => void;
+};
+
+type LoginData = {
+  email: string;
+  password: string;
+};
 
 const RegisterForm = (props: RegisterFormProps) => {
+  const { onSubmit, ...rest } = props;
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = () => {
+    onSubmit({ email, password });
+  };
+
   return (
-    <FormControl display={'flex'} flexDirection={'column'} gap={6} {...props}>
-      <Box display={'flex'} flexDirection={'row'} gap={6}>
-        <FormLabel>이메일</FormLabel>
-        <CustomInput />
-      </Box>
-      <Box display={'flex'} flexDirection={'row'} gap={6}>
-        <FormLabel>비밀번호</FormLabel>
-        <CustomInput type="password" />
-      </Box>
-      <CustomButton>회원가입</CustomButton>
-    </FormControl>
+    <form onSubmit={handleSubmit}>
+      <FormControl display={'flex'} flexDirection={'column'} gap={6} {...rest}>
+        <Box display={'flex'} flexDirection={'row'} gap={6}>
+          <FormLabel>이메일</FormLabel>
+          <CustomInput
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Box>
+        <Box display={'flex'} flexDirection={'row'} gap={6}>
+          <FormLabel>비밀번호</FormLabel>
+          <CustomInput
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Box>
+        <CustomButton>회원가입</CustomButton>
+      </FormControl>
+    </form>
   );
 };
 
