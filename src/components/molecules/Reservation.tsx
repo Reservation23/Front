e76dea8b443afe15143fbe6a45f5e96c15bf9a83
bkdragon0228 <rlaqjarb2067@naver.com>
2023-09-02 16:42:06 +0900

    feat : 예약 상세 페이지 라우팅

--- src/components/molecules/Reservation.tsx
+++ src/components/molecules/Reservation.tsx
@@ -7,21 +7,29 @@ import { BoxProps, Tr, Td } from '@chakra-ui/react';
 import CustomButton from '../atoms/Button';
 
 const ReservationItem: React.FC<
-  ReservationItemProps & { style?: BoxProps }
-> = ({ description, location, memberId, name, storeId, style }) => {
+  ReservationItemProps & {
+    style?: BoxProps;
+    index: number;
+    onClick: (id: number) => void;
+  }
+> = ({
+  description,
+  location,
+  memberId,
+  name,
+  storeId,
+  style,
+  index,
+  onClick,
+}) => {
   return (
-    <Tr
-      display={'grid'}
-      gridTemplateColumns={'1fr 1fr 1fr 3fr 1fr'}
-      alignItems={'center'}
-      {...style}
-    >
-      <Td>1</Td>
+    <Tr display={'grid'} gridTemplateColumns={'1fr 1fr 1fr 3fr 1fr'} {...style}>
+      <Td>{index}</Td>
       <Td>{name}</Td>
       <Td>{location}</Td>
       <Td>{description}</Td>
       <Td>
-        <CustomButton>예약</CustomButton>
+        <CustomButton onClick={() => onClick(storeId)}>예약</CustomButton>
       </Td>
     </Tr>
   );
--- src/components/organisms/ReservationList.tsx
+++ src/components/organisms/ReservationList.tsx
@@ -7,15 +7,21 @@ import { getReservation } from '../../store/_reducer/reservation';
 import { Table, TableProps } from '@chakra-ui/react';
 
 import ReservationItem from '../molecules/Reservation';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   ListStyle?: TableProps;
 }
 
 const ReservationList = (props: Props) => {
+  const navigate = useNavigate();
   const dispatch = useAppThunkDispatch();
 
   const data = useAppSelector((state) => state.reservation);
+
+  const onClick = (id: number) => {
+    navigate(`/reservation/${id}`);
+  };
 
   useEffect(() => {
     dispatch(getReservation());
@@ -23,8 +29,13 @@ const ReservationList = (props: Props) => {
 
   return (
     <Table display={'flex'} flexDirection={'column'} {...props.ListStyle}>
-      {data?.data.map((item) => (
-        <ReservationItem key={item?.storeId} {...item} />
+      {data?.data.map((item, index) => (
+        <ReservationItem
+          key={item?.storeId}
+          index={index + 1}
+          onClick={onClick}
+          {...item}
+        />
       ))}
     </Table>
   );
