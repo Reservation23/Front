7f761e599694b3bd3c98bf8f4948c4ab4fa134f6
bkdragon0228 <rlaqjarb2067@naver.com>
2023-09-02 17:22:56 +0900

    feat : Header에 서비스명 추가

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 
 import { useAppSelector, useAppDispatch } from '../../store/index';
 
@@ -28,10 +28,13 @@ const Header = () => {
       padding={'0 20px'}
       borderBottom={'1px solid black'}
     >
-      <span>{user.username} 님 환영합니다.</span>
-      <CustomButton type="button" onClick={onClickLogOut}>
-        로그아웃
-      </CustomButton>
+      <Heading size={'md'}>매장 예약 서비스</Heading>
+      <Box display={'flex'} alignItems={'center'} gap={6}>
+        <span>{user.username} 님 환영합니다</span>
+        <CustomButton type="button" onClick={onClickLogOut}>
+          로그아웃
+        </CustomButton>
+      </Box>
     </Box>
   );
 };
